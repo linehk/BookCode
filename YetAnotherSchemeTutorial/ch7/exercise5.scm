9436512897afc1198b293c3ea8bacf9f63167d78
sulinehk <sulinehk@gmail.com>
2023-12-25 12:55:38 +0800

    finish YetAnotherSchemeTutorial ch7

---
+++ YetAnotherSchemeTutorial/ch7/exercise5.scm
@@ -0,0 +1,14 @@
+; 练习5
+; 
+; 用do表达式重写练习2。
+
+; do 就像普通的 for 循环，分成变量、初始化、步进条件，然后是判断、结果
+(define (my-reverse-do ls)
+    (do ( (ls0 ls (cdr ls0)) (ls1 '() (cons (car ls0) ls1) ) ) ((null? ls0) ls1 ) ))
+
+(define (my-sum-do ls)
+    (do ((ls0 ls (cdr ls0)) (n 0 (+ n (car ls0)))) ((null? ls0) n)))
+
+(define (my-string->integer-do str)
+    (do ((ls0 (string->list str) (cdr ls0)) (n 0 (+ (- (char->integer (car ls0)) 48)
+           (* n 10)))) ((null? ls0) n)))
